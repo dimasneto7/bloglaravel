5aed2d920f943d33dc48462da5e2903dfb395f85
Dimas Neto <dimas_fcn@outlook.com>
2022-08-12 10:58:01 -0300

    tailwind

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
 </head>
-<body>
-    <div class="content">
+<body class="bg-gray-50">
+    <div class="container mx-auto py-8">
         @yield('content')
     </div>
 </body>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-    ],
+    plugins: [laravel(["resources/css/app.css", "resources/js/app.js"])],
 });
+
+// export default defineConfig({
+//     server: {
+//         host: "localhost",
+//     },
+//     plugins: [
+//         laravel({
+//             input: ["resources/css/app.css", "resources/js/app.js"],
+//             refresh: true,
+//         }),
+//     ],
+// });
